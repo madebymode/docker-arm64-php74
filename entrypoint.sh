26ce16c556df9e4589a2d87398d49e0d765b39ef
Troy McCall <troyxmccall@gmail.com>
2023-04-15 20:18:05 -0400

    let's do one thing at a time troy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-# Get the host UID and GID (prioritize HOST_USER_UID/GID, default to 1000 if not provided)
-USER_UID=${HOST_USER_UID:-${HOST_UID:-1000}}
-USER_GID=${HOST_USER_GID:-${HOST_GID:-1000}}
-
-# Update the www-data user and group to match the host UID and GID
-deluser www-data
-addgroup -g $USER_GID -S www-data
-adduser -u $USER_UID -D -S -G www-data www-data
-
 # Enable or disable opcache based on HOST_ENV
 if [ "$HOST_ENV" = "production" ]; then
     cp -f "$PHP_INI_DIR/conf.d/docker-php-ext-opcache.ini" "$PHP_INI_DIR/conf.d/"
@@ -18,10 +9,4 @@ else
     ln -sf "$PHP_INI_DIR/php.ini-development" "$PHP_INI_DIR/php.ini"
 fi
 
-# Check if EXEC_AS_ROOT is set to 1 and switch to root user if true
-if [ "$EXEC_AS_ROOT" = "1" ]; then
-  exec "$@"
-else
-  # Switch to the www-data user
-  su -s /bin/sh -c 'exec "$0" "$@"' www-data -- "$@"
-fi
+exec "$@"
