ebe8347fd3863ae335b882dbb3b382e8b9f18b82
Troy McCall <troyxmccall@gmail.com>
2023-04-15 20:43:01 -0400

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,14 @@
 
 # Enable or disable opcache based on HOST_ENV
 if [ "$HOST_ENV" = "production" ]; then
-    # rm -f "$PHP_INI_DIR/conf.d/docker-php-ext-opcache.ini"
-    # cp "$PHP_INI_DIR/conf.d/docker-php-ext-opcache.default" "$PHP_INI_DIR/conf.d/docker-php-ext-opcache.ini"
+    if [ -f "$PHP_INI_DIR/conf.d/docker-php-ext-opcache.disabled" ]; then
+        mv "$PHP_INI_DIR/conf.d/docker-php-ext-opcache.disabled" "$PHP_INI_DIR/conf.d/docker-php-ext-opcache.ini"
+    fi
     ln -sf "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/conf.d/php.ini"
 else
-    # rm -f "$PHP_INI_DIR/conf.d/docker-php-ext-opcache.ini"
+    if [ -f "$PHP_INI_DIR/conf.d/docker-php-ext-opcache.ini" ]; then
+        mv "$PHP_INI_DIR/conf.d/docker-php-ext-opcache.ini" "$PHP_INI_DIR/conf.d/docker-php-ext-opcache.disabled"
+    fi
     ln -sf "$PHP_INI_DIR/php.ini-development" "$PHP_INI_DIR/conf.d/php.ini"
 fi
 
